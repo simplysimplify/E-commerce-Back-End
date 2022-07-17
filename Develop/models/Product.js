e961ef163114eac34da47f266c24339cddc873f7
Sev <simplysimplifyy@gmail.com>
2022-07-17 14:11:11 -0600

    Finished routes, seeding, models and all functionality

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { STRING } = require("mysql2/lib/constants/types");
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -37,7 +38,10 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
-      references:
+      references: {
+        model: "category",
+        key: "id",
+      },
     }
   },
   {
